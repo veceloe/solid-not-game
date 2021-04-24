d35b388ed3485be7d906631b68916877dafdaebf
veceloe <81566766+veceloe@users.noreply.github.com>
2021-04-24 19:46:04 +0300

    game created

---
+++ core/src/com/solid/not/game/solidNotGame.java
@@ -0,0 +1,81 @@
+package com.solid.not.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class solidNotGame extends ApplicationAdapter {
+
+	public PerspectiveCamera cam;
+	public ModelBatch modelBatch;
+	public Model model;
+	public ModelInstance instance;
+	public Environment environment;
+	public CameraInputController cic;
+	public Texture obama;
+	
+	@Override
+	public void create () {
+		obama = new Texture("core\\assets\\obamium.jpg");
+		modelBatch = new ModelBatch();
+		cam = new PerspectiveCamera(90, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		cic = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(cic);
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f,
+				obama.draw(,0,0)
+		new Material(),
+		VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
+	}
+
+
+	@Override
+	public void render() {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		modelBatch.begin(cam);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
+
+		cic.update();
+	}
+
+	@Override
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
+
+	}
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void resize (int width, int height) {
+	}
+
+	@Override
+	public void pause () {
+	}
+}
+
